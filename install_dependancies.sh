a05cc9467beff5f5364b2e8bd0e54af41732c3f5
david <david@mediaodyssee.com>
2012-04-08 15:16:32 +0200

    install dependancies script

---
+++ install_dependancies.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+aptitude install python python-django python-pip dmtcp python-zmq
+pip install south
+pip install django-tastypie
+pip install pexpect
+pip install python-daemon
+
+
